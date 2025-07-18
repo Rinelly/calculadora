687fdf550520980fc66c00c47fd46bdbd0fc8529
Rinelly <rinellyvasconcelos@gmail.com>
2025-07-18 13:01:53 -0300

    Adicionei a função que impede o usuário de adicionar o evento de teclado de ação padrão, e estilização no css para esconder o cursor.

--- app.js
+++ app.js
@@ -21,3 +21,17 @@ function inverterSinal() {
     
     document.querySelector(".display").value = eval(valorDisplay) * -1;
 }
+
+//Adicionar o evento de teclado para impedir a ação padrão
+
+window.onload = function() {
+    const display = document.querySelector(".display" );
+    // Impede o usuário de digitar diretamente no campo de entrada
+    display.addEventListener("keydown", function(event) {
+        event.preventDefault();
+    });
+    // Impede o usuário de colar texto no campo de entrada
+    display.addEventListener("paste", function(event) {
+        event.preventDefault();
+    });
+}
