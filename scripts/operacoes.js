50de5f793e06668e9aad523dbbaadf30beecae94
Rinelly <rinellyvasconcelos@gmail.com>
2025-07-22 14:55:37 -0300

    Mudando o operador  ponto (.) para vírgula (,).

--- scripts/operacoes.js
+++ scripts/operacoes.js
@@ -17,10 +17,13 @@ export function limpaTela() {
 export function calcular() {
     const display = document.querySelector(".display");
     if (display) {
-        const expressao = display.value;
+        const expressaoOriginal = display.value;
+        const expressao = expressaoOriginal.replace(/,/g, '.'); // Substitui vírgulas por pontos
         try {
-            display.value = eval(display.value);
-            adicionarHistorico(expressao, display.value);
+            const resultado = eval(expressao);
+            display.value = String(resultado).replace('.', ','); // Converte o resultado para string e substitui ponto por vírgula
+            // Adiciona o histórico
+            adicionarHistorico(expressaoOriginal, display.value);
         } catch (e) {
             display.value = "Erro";
         }
